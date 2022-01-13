eff13a9f8acb0470f513b1e0ff5caae3e00f2559
Legati0 <yannick.zimmermann@gmx.de>
2022-01-12 16:16:45 -1000

    revision hw1

--- hw01/2_4.cc
+++ hw01/2_4.cc
@@ -1,15 +1,19 @@
 //#include <iostream>
 #include <cstdio>
 
+double acc(double v) {
+	return -9.81 + v * v * 0.5 * 1 * 1 * 1.2 / 80;
+}
+
 int main() {
-	double y = 1000;
-	double v = 0;
-	double dt = 1;
-	double t = 0;
-	while (y > 0) {
-		double a = -9.81 + v*v*0.5*1*1*1.2/80;
-		y += v*dt;
-		v += a*dt;
+	double y = 1000.0;	// current height
+	double v = 0.0;		// current speed
+	const double dt = 1.0;	// time difference between recalc
+	double t = 0.0;		// seconds passed
+	while (y > 0.0) {
+		double a = acc(v);	// calc current acc
+		y += v * dt;		// recalc y
+		v += a * dt;		// recalc v
 		t += dt;
 	}
 	//std::cout << t << std::endl;
