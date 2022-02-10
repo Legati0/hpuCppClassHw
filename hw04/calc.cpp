b73b6df28f31f67655d1f4cf4c5716373a155e06
Legati0 <yannick.zimmermann@gmx.de>
2022-02-09 17:47:12 -1000

    revision hw04

--- hw04/calc.cpp
+++ hw04/calc.cpp
@@ -5,55 +5,53 @@
 #include "calc.h"
 
 
-void readFourccName(FILE* fp, char* fourcc) {
+bool readFourccName(FILE* fp, char* fourcc) {
     // read fourcc name
-    fread(fourcc, sizeof(char), 4, fp);
+    if (!fread(fourcc, sizeof(char), 4, fp))
+        return false;
     fourcc[4] = '\0';
     printf("FOURCC: %s\n", fourcc);
+    return true;
 }
 
 
-unsigned char* readFourccContents(FILE* fp, unsigned int* size) {
-    // read size
-    char sizeArr[4];
-    fread(sizeArr, sizeof(char), 4, fp);
-    //printf("%d%d%d%d\n", sizeArr[0], sizeArr[1], sizeArr[2], sizeArr[3]);
-    unsigned int* n = (unsigned int*) sizeArr;
-    printf("Size: %d\n", *n);
+void readFourccSize(FILE* fp, int* size) {
+    // read fourcc size
+    fread(size, sizeof(int), 1, fp);
+}
 
-    unsigned char* contents = new unsigned char[*n];
-    fread(contents, sizeof(unsigned char), *n, fp);
-    *size = *n;
-    return contents;
+
+void readFourccContents(FILE* fp, int size, unsigned char* contents) {
+    fread(contents, sizeof(char), size, fp);
 }
 
 
 void parseFmt(unsigned char* contents, unsigned int size, Format* format) {
-    format->format = (uint16_t*) (contents);
-    format->nchannels = (uint16_t*) (contents+2);
-    format->rate = (uint32_t*) (contents+4);
-    format->byte_rate = (uint32_t*) (contents+8);
-    format->align = (uint16_t*) (contents+12);
-    format->bits_per_sample = (uint16_t*) (contents+14);
+    format->format = *((uint16_t*) (contents));
+    format->nchannels = *((uint16_t*) (contents+2));
+    format->rate = *((uint32_t*) (contents+4));
+    format->byte_rate = *((uint32_t*) (contents+8));
+    format->align = *((uint16_t*) (contents+12));
+    format->bits_per_sample = *((uint16_t*) (contents+14));
     // size should be 16, bits_per_sample has offset of 14 
     // and size of 2 bytes, so everything is good
-    printf("format: %u\n", *format->format);
-    printf("nchannels: %u\n", *format->nchannels);
-    printf("rate: %u\n", *format->rate);
-    printf("byte rate: %u\n", *format->byte_rate);
-    printf("align: %u\n", *format->align);
-    printf("bits_per_sample: %u\n", *format->bits_per_sample);
+    printf("format: %u\n", format->format);
+    printf("nchannels: %u\n", format->nchannels);
+    printf("rate: %u\n", format->rate);
+    printf("byte rate: %u\n", format->byte_rate);
+    printf("align: %u\n", format->align);
+    printf("bits_per_sample: %u\n", format->bits_per_sample);
 }
 
 
 void parseData(unsigned char* contents, unsigned int size, const Format* format) {
     // works same as /2 with 8|16 and is faster than division
-    unsigned int bytesPerSample = *format->bits_per_sample % 7; 
+    unsigned int bytesPerSample = format->bits_per_sample % 7; 
     // iterate over contents and print them
     for (unsigned int offset = 0; offset < size; offset += bytesPerSample) {
         int16_t* byte = (int16_t*) (contents + offset);
         printf("%d", *byte);
-        if (*format->nchannels == 2) {
+        if (format->nchannels == 2) {
             offset += bytesPerSample;
             int16_t* byteC2 = (int16_t*) (contents + offset);
             printf("\t|\t%d", *byteC2);
--- hw04/calc.h
+++ hw04/calc.h
@@ -6,17 +6,18 @@
 
 
 struct Format {
-    uint16_t* format;
-    uint16_t* nchannels;
-    uint32_t* rate;
-    uint32_t* byte_rate;
-    uint16_t* align;
-    uint16_t* bits_per_sample;
+    uint16_t format;
+    uint16_t nchannels;
+    uint32_t rate;
+    uint32_t byte_rate;
+    uint16_t align;
+    uint16_t bits_per_sample;
 };
 
-void readFourccName(FILE* fp, char* fourcc);
 
-unsigned char* readFourccContents(FILE* fp, unsigned int* size);
+bool readFourccName(FILE* fp, char* fourcc);
+void readFourccSize(FILE* fp, int* size);
+void readFourccContents(FILE* fp, int size, unsigned char* contents);
 
 void parseFmt(unsigned char* contents, unsigned int size, Format* format);
 void parseData(unsigned char* contents, unsigned int size, const Format* format);
