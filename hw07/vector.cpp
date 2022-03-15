c74a9deff25d11aa2aa3240928f86b3e108f2cb4
Legati0 <yannick.zimmermann@gmx.de>
2022-03-14 15:59:21 -1000

    revision 3 hw07

--- hw07/vector.cpp
+++ hw07/vector.cpp
@@ -3,25 +3,30 @@
 
 
 Vector::Vector(double x, double y, double z) : x(x), y(y), z(z) {}
+
 Vector::~Vector() {}
 
-Vector Vector::operator+(Vector& vec) {
+Vector Vector::operator+(Vector& vec) const {
 
 	return Vector(this->x + vec.x, this->y + vec.y, this->z + vec.z);
 }
-Vector Vector::operator-(Vector& vec) {
+
+Vector Vector::operator-(Vector& vec) const {
 	return Vector(this->x - vec.x, this->y - vec.y, this->z - vec.z);
 }
+
 Vector& Vector::operator+=(Vector& vec) {
 	this->x += vec.x;
 	this->y += vec.y;
 	this->z += vec.z;
 	return *this;
 }
-Vector Vector::operator*(double a) {
+
+Vector Vector::operator*(double a) const {
 	return Vector(this->x * a, this->y * a, this->z * a);
 }
-double Vector::operator*(Vector& vec) {
+
+double Vector::operator*(Vector& vec) const {
 	return this->x * vec.x + this->y * vec.y + this->z * vec.z;
 }
 
--- hw07/vector.h
+++ hw07/vector.h
@@ -11,11 +11,11 @@ public:
 	Vector(double x, double y, double z);
 	virtual ~Vector();
 
-	Vector operator+(Vector& vec);
-	Vector operator-(Vector& vec);
+	Vector operator+(Vector& vec) const;
+	Vector operator-(Vector& vec) const;
 	Vector& operator+=(Vector& vec);
-	Vector operator*(double a);
-	double operator*(Vector& vec);
+	Vector operator*(double a) const;
+	double operator*(Vector& vec) const;
 	friend std::ostream& operator<< (std::ostream& out, const Vector& vec); 
 
 };
